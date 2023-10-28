28d39e495029e144c01cdb54bb6132130e827abc
Emily Erskine <s00209630@mail.itsligo.ie>
2023-10-28 10:49:21 +0100

    Background colour fixes - Logo match up

--- src/CommonComponents/Header/Header.jsx
+++ src/CommonComponents/Header/Header.jsx
@@ -5,15 +5,15 @@ export const Header = () => {
     <div style={{ display: 'grid', gridTemplateColumns: '50% 50%', padding: '2% 5%' }}>
       <div style={{ textAlign: 'left' }}>
         <a href="/">
-          <img src='./AVConLogoWhite.jpeg' alt="AV Con" width={'35%'} />
+          <img src='./AVConLogoWhite.png' alt="AV Con" width={'35%'} />
         </a>
       </div>
       <div style={{ textAlign: 'right', padding:'12% 0 0 0' }}>
         <div style={{ display: 'grid', gridTemplateColumns: '25% 25% 25% 25%', gap: '2%'}}>
-          <a href="./Event">Event Info</a>
-          <a href="./About">About AVCon</a>
-          <a href="./Contact">Contact Us</a>
-          <a href="./Register">Register</a>
+          <a href="./Event">EVENT INFO</a>
+          <a href="./About">ABOUT AVCON</a>
+          <a href="./Contact">CONTACT US</a>
+          <a href="./Register">REGISTER</a>
         </div>
       </div>
     </div>
