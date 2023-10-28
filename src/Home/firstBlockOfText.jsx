dbd40f523dc6996b794059f6eb96f76366c8db80
Emily Erskine <s00209630@mail.itsligo.ie>
2023-10-28 13:38:47 +0100

    Adding more details to the HomePage

--- src/Home/firstBlockOfText.jsx
+++ src/Home/firstBlockOfText.jsx
@@ -11,7 +11,7 @@ export default function FirstImageAndText() {
                 <img
                   src="https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?cs=srgb&dl=pexels-pixabay-46148.jpg&fm=jpg"
                   alt="Airplane"
-                  style={{ width: '80%' }}
+                  style={{ width: '80%', mixBlendMode: 'luminosity' }}
                 />
               </div>
           </Grid>
