58e56a29fa40e4f01228800cdf1c7b6f69a5907e
Emily Erskine <s00209630@mail.itsligo.ie>
2023-10-28 13:02:30 +0100

    Getting the Social Media Links into the Footer

--- src/CommonComponents/Footer/Footer.jsx
+++ src/CommonComponents/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import SocialMediaLinks from './SocialMediaLinks';
 
 export const Footer = () => (
-    <div style={{padding: '5%'}}>
-        <p style={{fontSize: '10px'}}>Copyright © 2023 Emily-Huong Erskine</p>
-    </div>
+    <div style={{ display: 'grid', gridTemplateColumns: '50% 50%', padding: '5% 10% 0% 5%' }}>
+        <div style={{ textAlign: 'left', padding: '5%'}}>
+            <p style={{fontSize: '10px'}}>Copyright © 2023 Emily-Huong Erskine</p>
+        </div>
+        <div style={{ textAlign: 'right', padding:'5% 0 0 0' }}>
+            <SocialMediaLinks />
+        </div>
+  </div>
 );
---
+++ src/CommonComponents/Footer/SocialMediaLinks.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faFacebook,
+    faTwitter,
+    faInstagram
+  } from "@fortawesome/free-brands-svg-icons";
+
+export default function SocialMediaLinks() {
+  return (
+    <div class="social-container">
+      <a href="https://www.facebook.com/profile.php?id=61552633058659&mibextid=ZbWKwL"
+        className="facebook social"  style={{padding: '2%'}}>
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href="https://www.twitter.com/" className="twitter social"  style={{padding: '2%'}}>
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a href="https://www.instagram.com/"
+        className="instagram social"  style={{padding: '2%'}}>
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+      
+    </div>
+  );
+}
