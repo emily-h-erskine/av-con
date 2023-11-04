fa85a83903d73612d88233e81f828755abfbdcc9
Emily Erskine <s00209630@mail.itsligo.ie>
2023-11-04 01:32:05 +0000

    I always forget something :(

--- src/Contact/contact.jsx
+++ src/Contact/contact.jsx
@@ -12,7 +12,7 @@ export default function Contact() {
                 <img
                   src="https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?cs=srgb&dl=pexels-pixabay-46148.jpg&fm=jpg"
                   alt="Airplane"
-                  style={{ width: '80%' }}
+                  style={{ width: '80%', mixBlendMode: 'luminosity' }}
                 />
               </div>
             </Grid>
