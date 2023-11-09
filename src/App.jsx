c7cfa23330d49029c8ee13555780540168abba3f
Emily Erskine <s00209630@mail.itsligo.ie>
2023-11-09 19:25:38 +0000

    Getting the new routes (News isnt working so sad)

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ import Home from './Home/home';
 import Event from './Event/event';
 import Register from './Register/register';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EventSchedule from "./EventSchedule/eventSchedule";
+import FlightSim from "./FlightSim/flightSim";
+import News from "./News/newsUpdates";
+import SpeakersPresenters from "./SpeakersPresenters/speakersPresenters";
 
 export default function App() {
   return (
@@ -21,6 +25,10 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/eventSchedule" element={<EventSchedule />} />
+          <Route path="/flightSim" element={<FlightSim />} />
+          <Route path="/newsUpdates" element={<News />} />
+          <Route path="/speakersPresenters" element={<SpeakersPresenters />} />
         </Routes>
       </RootLayout>
       <Footer />
---
+++ src/SpeakersPresenters/speakersPresenters.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+export default function SpeakersPresenters() {
+  return (
+      <main className="flex min-h-screen flex-col justify-between p-24">
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <img
+                  src="https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?cs=srgb&dl=pexels-pixabay-46148.jpg&fm=jpg"
+                  alt="Airplane"
+                  style={{ width: '80%', mixBlendMode: 'luminosity'}}
+                />
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div>
+                <p>Speakers and Presenters</p>
+              </div>
+            </Grid>
+          </Grid>
+        </Box>
+      </main>
+  );
+}
