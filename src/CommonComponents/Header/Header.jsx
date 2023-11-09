5772fe7f44e57a5db8bb70a38a5beafd6cf01f4c
Emily Erskine <s00209630@mail.itsligo.ie>
2023-11-09 19:06:11 +0000

    Sorting Dropdowns : Need to Add Icons/Indicators

--- src/CommonComponents/Header/Header.jsx
+++ src/CommonComponents/Header/Header.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Header = () => {
+  const [showEventDropdown, setShowEventDropdown] = useState(false);
+  const [showAboutDropdown, setShowAboutDropdown] = useState(false);
+
+  const handleEventDropdownToggle = () => {
+    setShowEventDropdown(!showEventDropdown);
+  };
+
+  const handleAboutDropdownToggle = () => {
+    setShowAboutDropdown(!showAboutDropdown);
+  };
+
   return (
     <div style={{ display: 'grid', gridTemplateColumns: '50% 50%', padding: '0% 10% 0% 5%' }}>
       <div style={{ textAlign: 'left' }}>
@@ -9,9 +20,35 @@ export const Header = () => {
         </a>
       </div>
       <div style={{ textAlign: 'right', padding:'15% 0 0 0' }}>
-        <div style={{ display: 'grid', gridTemplateColumns: '25% 25% 25% 25%', gap: '2%'}}>
-          <a href="./Event">EVENT INFO</a>
-          <a href="./About">ABOUT AVCON</a>
+        <div style={{ display: 'grid', gridTemplateColumns: '25% 25% 25% 25%', gap: '2%' }}>
+          <div
+            style={{ position: 'relative', display: 'inline-block' }}
+            onMouseEnter={handleEventDropdownToggle}
+            onMouseLeave={handleEventDropdownToggle}
+          >
+            <a href="./Event">EVENT INFO</a>
+            {showEventDropdown && (
+              <div style={{ position: 'absolute', top: '100%', left: 0, backgroundColor: '#07101D', padding: '10px' }}>
+                <a href="./EventSchedule">EVENT SCHEDULE</a>
+                <br/>
+                <a href="./SpeakersPresenters">SPEAKERS &amp; PRESENTERS</a>
+                <br/>
+                <a href="./FlightSim">24-HOUR FLIGHT SIM COMPETITION</a>
+              </div>
+            )}
+          </div>
+          <div
+            style={{ position: 'relative', display: 'inline-block' }}
+            onMouseEnter={handleAboutDropdownToggle}
+            onMouseLeave={handleAboutDropdownToggle}
+          >
+            <a href="./About">ABOUT AVCON</a>
+            {showAboutDropdown && (
+              <div style={{ position: 'absolute', top: '100%', left: 0, backgroundColor: '#07101D', padding: '10px' }}>
+                <a href="./News">NEWS &amp; UPDATES </a>
+              </div>
+            )}
+          </div>
           <a href="./Contact">CONTACT US</a>
           <a href="./Register">REGISTER</a>
         </div>
