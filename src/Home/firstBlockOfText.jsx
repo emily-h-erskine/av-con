3000fbfcc8f2c99b25d5000888f29b2d1ffede01
Emily Erskine <s00209630@mail.itsligo.ie>
2023-11-14 09:56:11 +0000

    Home page is fully responsive

--- src/Home/firstBlockOfText.jsx
+++ src/Home/firstBlockOfText.jsx
@@ -4,30 +4,30 @@ import Grid from '@mui/material/Grid';
 
 export default function FirstImageAndText() {
   return (
-      <Box style={{padding:" 0 5% 0 0"}}>
-        <Grid container >
-          <Grid item xs={7}>
-              <div style={{ textAlign: 'center' }}>
-                <img
-                  src="https://i.ytimg.com/vi/etSlzjGCiKg/maxresdefault.jpg"
-                  alt="Shannon Airport Image"
-                  style={{ width: '80%', mixBlendMode: 'luminosity' }}
-                />
-              </div>
-          </Grid>
-          <Grid item xs={5}>
-              <div>
-                <h1>
-                  See you at AvCon!!
-                  <br /><br />
-                  Where the future of aviation takes flight!
-                </h1>
-                <h3 style={{textAlign: 'right'}}>
-                  This two-day extravaganza opens the doors to a world of thrilling opportunities and cutting-edge technology in aviation. 
-                </h3>
-              </div>
-          </Grid>
+    <Box style={{ padding: "0 5% 0 0" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={7}>
+          <div style={{ textAlign: 'center' }}>
+            <img
+              src="https://i.ytimg.com/vi/etSlzjGCiKg/maxresdefault.jpg"
+              alt="Shannon Airport Image"
+              style={{ width: '100%', maxWidth: '600px', mixBlendMode: 'luminosity' }}
+            />
+          </div>
         </Grid>
-      </Box>
+        <Grid item xs={12} sm={5}>
+          <div>
+            <h1>
+              See you at AvCon!!
+              <br /><br />
+              Where the future of aviation takes flight!
+            </h1>
+            <h3 style={{ textAlign: 'right' }}>
+              This two-day extravaganza opens the doors to a world of thrilling opportunities and cutting-edge technology in aviation.
+            </h3>
+          </div>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
--- src/Home/secondBlockOfText.jsx
+++ src/Home/secondBlockOfText.jsx
@@ -4,41 +4,41 @@ import Grid from '@mui/material/Grid';
 
 export default function SecondImageAndText() {
   return (
-      <Box style={{padding:" 0 0 0 5%"}}>
-        <Grid container >
-          <Grid item xs={5}>
-              <div>
-                  <b>Highlights:</b>
-                  <ul>
-                    <li>Explore exciting aviation careers with industry experts.</li>
-                    <li>Get a glimpse of the latest aviation technology.</li>
-                    <li>Listen to insightful talks from industry leaders.</li>
-                    <li>Visit booths showcasing innovations and career opportunities.</li>
-                  </ul>
-                  <b>24-Hour Flight Sim Competition:</b>
-                  <ul>
-                    <li>Thrilling non-stop flight simulation from 12 pm to 12 pm.</li>
-                    <li>Support the Shannon Aviation Museum with your participation.</li>
-                    <li>Form a team of 4-8 for a round-the-world flight challenge.</li>
-                    <li>Each team must raise €3000 to enter.</li>
-                    <li>Contact flightsim@avcon.ie to register your team.</li>
-                  </ul>
-                  <br/>
-                  <hr />
-                  <h4 style={{ textAlign: 'center' }}>Join us for an extraordinary event fusing education, entertainment, and fundraising. Don't miss your chance to be part of the aviation revolution!</h4>
-                  <hr/>
-              </div>
-          </Grid>
-          <Grid item xs={7}>
-              <div style={{ textAlign: 'center' }}>
-                <img
-                  src="https://www.atlanticaviation.ie/wp-content/uploads/2023/03/Shane-and-girls-scaled.jpg"
-                  alt="Sample AirCraft Tech Photo"
-                  style={{ width: '80%', mixBlendMode: 'luminosity' }}
-                />
-              </div>
-          </Grid>
+    <Box style={{ padding: "0 0 0 5%" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={5}>
+          <div>
+            <b>Highlights:</b>
+            <ul>
+              <li>Explore exciting aviation careers with industry experts.</li>
+              <li>Get a glimpse of the latest aviation technology.</li>
+              <li>Listen to insightful talks from industry leaders.</li>
+              <li>Visit booths showcasing innovations and career opportunities.</li>
+            </ul>
+            <b>24-Hour Flight Sim Competition:</b>
+            <ul>
+              <li>Thrilling non-stop flight simulation from 12 pm to 12 pm.</li>
+              <li>Support the Shannon Aviation Museum with your participation.</li>
+              <li>Form a team of 4-8 for a round-the-world flight challenge.</li>
+              <li>Each team must raise €3000 to enter.</li>
+              <li>Contact flightsim@avcon.ie to register your team.</li>
+            </ul>
+            <br />
+            <hr />
+            <h4 style={{ textAlign: 'center' }}>Join us for an extraordinary event fusing education, entertainment, and fundraising. Don't miss your chance to be part of the aviation revolution!</h4>
+            <hr />
+          </div>
         </Grid>
-      </Box>
+        <Grid item xs={12} sm={7}>
+          <div style={{ textAlign: 'center' }}>
+            <img
+              src="https://www.atlanticaviation.ie/wp-content/uploads/2023/03/Shane-and-girls-scaled.jpg"
+              alt="Sample AirCraft Tech Photo"
+              style={{ width: '100%', maxWidth: '600px', mixBlendMode: 'luminosity' }}
+            />
+          </div>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
