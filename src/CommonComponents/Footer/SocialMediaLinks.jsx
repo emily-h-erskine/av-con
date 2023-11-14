564450b83ba07b72d986cd93595cc7be57fffe53
Emily Erskine <s00209630@mail.itsligo.ie>
2023-11-14 13:45:58 +0000

    Updating Social media links

--- src/CommonComponents/Footer/SocialMediaLinks.jsx
+++ src/CommonComponents/Footer/SocialMediaLinks.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faFacebook,
-    faTwitter,
     faInstagram,
-    faTiktok
+    faTiktok,
+    faLinkedinIn
   } from "@fortawesome/free-brands-svg-icons";
 
 export default function SocialMediaLinks() {
   return (
     <div class="social-container">
-      <a href="https://www.facebook.com/profile.php?id=61552633058659&mibextid=ZbWKwL"
+      <a href="https://www.facebook.com/profile.php?id=61552633058659"
         className="facebook social"  style={{padding: '2%'}}>
         <FontAwesomeIcon icon={faFacebook} size="2x" />
       </a>
-      <a href="https://www.instagram.com/"
+      <a href="https://instagram.com/avcon_ie?igshid=eTNvcHN4MHdkZHJr"
         className="instagram social"  style={{padding: '2%'}}>
         <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a>
@@ -22,8 +22,8 @@ export default function SocialMediaLinks() {
         className="tictok social"  style={{padding: '2%'}}>
         <FontAwesomeIcon icon={faTiktok} size="2x" />
       </a>
-      <a href="https://www.twitter.com/" className="twitter social"  style={{padding: '2%'}}>
-        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      <a href="https://www.linkedin.com/in/tyhub?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="LinkedIn social"  style={{padding: '2%'}}>
+        <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
       </a>
       
     </div>
