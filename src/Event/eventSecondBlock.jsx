145795975b8b062519a3ef1c55bee6e58b3702a8
Emily Erskine <s00209630@mail.itsligo.ie>
2023-11-14 10:03:56 +0000

    Even page fully responsive

--- src/Event/eventSecondBlock.jsx
+++ src/Event/eventSecondBlock.jsx
@@ -4,42 +4,42 @@ import Grid from '@mui/material/Grid';
 
 export default function EventSecondBlock() {
   return (
-      <Box style={{padding:" 0 0 0 5%"}}>
-        <Grid container >
-            <Grid item xs={5}>
-            <div>
-                <b>Part 2 - 24-Hour Flight Sim Competition: </b>
-                <ul>
-                  <li><b>Non-stop Action:</b> Join us for a thrilling 24-hour flight simulation competition that kicks off at 12 pm on day 1 and concludes at 12 pm on day 2.</li>
-                  <li><b>Support Shannon Aviation Museum:</b> All proceeds from this competition will go to the Shannon Aviation Museum.</li>
-                  <li><b>Team Competition:</b> Teams of 4-8 participants will compete in 4 flight simulators, flying around the world without stopping.</li>
-                  <li><b>Fundraising Requirement:</b> Each team must raise a minimum of €3000 to enter.</li>
-                  <li><b>To Register a Team:</b> Contact our flight sim team at flightsim@avcon.ie.</li>
-                </ul>
-                <br />
-                <b>Exhibitor Inquiries:</b>
-                <p>If you are interested in showcasing your products or services at AvCon, please contact us at hello@avcon.ie.</p>
-            </div>
-            <hr/>
-            </Grid>
-            <Grid item xs={7}>
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <img
-                  src="https://www.dublinaerospace.com/site/uploads/sys_articles/62/Dublin%20Aerospace%20Recruiting%20Drive%20.jpg"
-                  alt="Airplane"
-                  style={{ width: '80%', mixBlendMode: 'luminosity'}}
-                />
-              </div>
-            </Grid>
+    <Box style={{ padding: "0 0 0 5%" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={5}>
+          <div>
+            <b>Part 2 - 24-Hour Flight Sim Competition: </b>
+            <ul>
+              <li><b>Non-stop Action:</b> Join us for a thrilling 24-hour flight simulation competition that kicks off at 12 pm on day 1 and concludes at 12 pm on day 2.</li>
+              <li><b>Support Shannon Aviation Museum:</b> All proceeds from this competition will go to the Shannon Aviation Museum.</li>
+              <li><b>Team Competition:</b> Teams of 4-8 participants will compete in 4 flight simulators, flying around the world without stopping.</li>
+              <li><b>Fundraising Requirement:</b> Each team must raise a minimum of €3000 to enter.</li>
+              <li><b>To Register a Team:</b> Contact our flight sim team at flightsim@avcon.ie.</li>
+            </ul>
+            <br />
+            <b>Exhibitor Inquiries:</b>
+            <p>If you are interested in showcasing your products or services at AvCon, please contact us at hello@avcon.ie.</p>
+            <hr />
+          </div>
         </Grid>
-        <br/>
-        <p>
-          AvCon promises to be an event like no other, offering a unique blend of education, entertainment, and fundraising for a great cause. Join us on [event dates] and be a part of the aviation revolution!
-          <br/><br/>
-          For the latest updates and information, visit our website at www.avcon.ie 
-          <br/>
-          Don't miss your chance to be a part of the future of aviation at AvCon!
-        </p>
-      </Box>
+        <Grid item xs={12} md={7}>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <img
+              src="https://www.dublinaerospace.com/site/uploads/sys_articles/62/Dublin%20Aerospace%20Recruiting%20Drive%20.jpg"
+              alt="Airplane"
+              style={{ width: '100%', maxWidth: '600px', mixBlendMode: 'luminosity' }}
+            />
+          </div>
+        </Grid>
+      </Grid>
+      <br />
+      <p>
+        AvCon promises to be an event like no other, offering a unique blend of education, entertainment, and fundraising for a great cause. Join us on [event dates] and be a part of the aviation revolution!
+        <br /><br />
+        For the latest updates and information, visit our website at www.avcon.ie
+        <br />
+        Don't miss your chance to be a part of the future of aviation at AvCon!
+      </p>
+    </Box>
   );
 }
--- src/Event/eventsFirstBlock.jsx
+++ src/Event/eventsFirstBlock.jsx
@@ -4,36 +4,36 @@ import Grid from '@mui/material/Grid';
 
 export default function EventFirstBlock() {
   return (
-      <Box style={{padding:" 0 5% 0 0"}}>
-        <Grid container >
-        <Grid item xs={6}>
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <img
-                  src="https://fullfatthings-keyaero.b-cdn.net/sites/keyaero/files/inline-images/Pilatus%20PC-9M%20%5B260%2C%20Irish%20Air%20Corps%20College%20-%20Flying%20Training%20School%2C%20Irish%20Air%20Corps%2C%20RAF%20Cosford%20Airshow%202016%5D%201.JPG"
-                  alt="Airplane"
-                  style={{ width: '80%', mixBlendMode: 'luminosity' }}
-                />
-              </div>
-            </Grid>
-            <Grid item xs={6}>
-              <div>
-                <b>Part 1 - Aviation Convention: </b>
-                <ul>
-                  <li><b>Discover Career Opportunities:</b> AvCon brings together the best and brightest minds in aviation to shed light on the exciting careers this industry has to offer.</li>
-                  <li><b>Tech Showcase:</b> Get a glimpse into the future with presentations on the latest aviation technology.</li>
-                  <li><b>Engaging Talks:</b> Hear from industry experts and thought leaders who will share their insights and experiences.</li>
-                  <li><b>Exhibitors:</b> Explore booths featuring the latest innovations, educational institutions, and career opportunities in aviation.</li>
-                </ul>
-                <b>Pricing:</b>
-                <ul>
-                  <li>School Groups: Free admission.</li>
-                  <li>Third-Level Students: Free with valid student cards.</li>
-                  <li>Individual Tickets: €10 per person.</li>
-                </ul>
-                <h5>To Register a School: Contact our dedicated school team at schools@avcon.ie.</h5>
-              </div>
-            </Grid>
+    <Box style={{ padding: "0 5% 0 0" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <img
+              src="https://fullfatthings-keyaero.b-cdn.net/sites/keyaero/files/inline-images/Pilatus%20PC-9M%20%5B260%2C%20Irish%20Air%20Corps%20College%20-%20Flying%20Training%20School%2C%20Irish%20Air%20Corps%2C%20RAF%20Cosford%20Airshow%202016%5D%201.JPG"
+              alt="Airplane"
+              style={{ width: '100%', maxWidth: '600px', mixBlendMode: 'luminosity' }}
+            />
+          </div>
         </Grid>
-      </Box>
+        <Grid item xs={12} md={6}>
+          <div>
+            <b>Part 1 - Aviation Convention: </b>
+            <ul>
+              <li><b>Discover Career Opportunities:</b> AvCon brings together the best and brightest minds in aviation to shed light on the exciting careers this industry has to offer.</li>
+              <li><b>Tech Showcase:</b> Get a glimpse into the future with presentations on the latest aviation technology.</li>
+              <li><b>Engaging Talks:</b> Hear from industry experts and thought leaders who will share their insights and experiences.</li>
+              <li><b>Exhibitors:</b> Explore booths featuring the latest innovations, educational institutions, and career opportunities in aviation.</li>
+            </ul>
+            <b>Pricing:</b>
+            <ul>
+              <li>School Groups: Free admission.</li>
+              <li>Third-Level Students: Free with valid student cards.</li>
+              <li>Individual Tickets: €10 per person.</li>
+            </ul>
+            <h5>To Register a School: Contact our dedicated school team at schools@avcon.ie.</h5>
+          </div>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
