a04817835c660c7e8ed9815379d3cf4e9cc67266
Emily Erskine <s00209630@mail.itsligo.ie>
2024-01-24 09:32:19 +0000

    Adding in the AVCon Promotional video

--- src/About/about.jsx
+++ src/About/about.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AboutTimeline from './aboutTimeline.jsx';
+import AVConPromoVideo from './The Future of aviation.mp4'
 
 export default function About() {
   const aboutAVCon = [
@@ -23,7 +24,22 @@ export default function About() {
     <main className="flex min-h-screen flex-col p-24">
       <div className="event-container">
         <h1 className="event-title">About AVCon</h1>
-        <AboutTimeline aboutAVCon={aboutAVCon} />
+        <div style={{textAlign: "center", padding:"2% 0 0 0"}}>
+        <iframe 
+          width="1120" 
+          height="630"
+          src={AVConPromoVideo}
+          title="AVCon Future of Aviation" 
+          frameborder="0" 
+          allow="accelerometer; 
+          autoplay; 
+          clipboard-write; 
+          encrypted-media; 
+          gyroscope; 
+          picture-in-picture; 
+          web-share" allowfullscreen />
+        </div>
+        {/* <AboutTimeline aboutAVCon={aboutAVCon} /> */}
       </div>
     </main>
   );
