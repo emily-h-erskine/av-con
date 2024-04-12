49f9c29eb2c2023cf22f80a04133537413e928ae
Emily Erskine <s00209630@mail.itsligo.ie>
2024-04-12 13:46:44 +0100

    Getting the Partners and Sponsors page up

--- src/SponsorsPartners/sponsor.jsx
+++ src/SponsorsPartners/sponsor.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 
-export default function Sponsor() {
+export default function Sponsor({ logoUrl, sponsorName }) {
   return (
-      <main className="flex min-h-screen flex-col justify-between p-24">
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <img
-                src="https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?cs=srgb&dl=pexels-pixabay-46148.jpg&fm=jpg"
-                alt="Airplane"
-                style={{ width: '80%', mixBlendMode: 'luminosity'}}
-            />
-        </div>
-        <h5 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Sponsor Name</h5>
-      </main>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <img
+        src={logoUrl}
+        alt="Sponsor Logo"
+        style={{ width: '80%'}}
+      />
+      <h5>{sponsorName}</h5>
+    </div>
   );
 }
--- src/SponsorsPartners/sponsorsPartners.jsx
+++ src/SponsorsPartners/sponsorsPartners.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Sponsor from "./sponsor";
+import AirCorpLogo from './SponsorImages/AirCorpLogo.jpg';
+import HireUpLogo from './SponsorImages/HireUpLogo.jpg';
+import ShannonAirportLogo from './SponsorImages/ShannonAirportLogo.jpg';
+import TYHubLogo from './SponsorImages/TYHubLogo.jpg';
+import ODohertysLogo from './SponsorImages/ODohertysLogo.jpg';
 
 export default function SponsorsPartners() {
   return (
@@ -13,19 +18,35 @@ export default function SponsorsPartners() {
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
           <Grid item xs={12} md={3}>
-            <Sponsor />
+            <Sponsor
+              logoUrl={AirCorpLogo}
+              sponsorName="Irish Air Corps"
+            />
           </Grid>
           <Grid item xs={12} md={3}>
-            <Sponsor />
+            <Sponsor
+              logoUrl={HireUpLogo}
+              sponsorName="Hire Up"
+            />
           </Grid>
           <Grid item xs={12} md={3}>
-            <Sponsor />
+            <Sponsor
+              logoUrl={ShannonAirportLogo}
+              sponsorName="Shannon Airport"
+            />
           </Grid>
           <Grid item xs={12} md={3}>
-            <Sponsor />
+            <Sponsor
+              logoUrl={TYHubLogo}
+              sponsorName="TY Hub"
+            />
           </Grid>
         </Grid>
       </Box>
+      <Sponsor
+        logoUrl={ODohertysLogo}
+        sponsorName="O'Doherty Media Group"
+      />
     </main>
   );
 }
