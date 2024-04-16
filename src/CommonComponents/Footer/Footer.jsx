887c3725df55b8f594b0e7c66bc14a226da4e095
Emily Erskine <s00209630@mail.itsligo.ie>
2024-04-16 09:44:13 +0100

    Try to fix the footer, might have to go to Plan C

--- src/CommonComponents/Footer/Footer.jsx
+++ src/CommonComponents/Footer/Footer.jsx
@@ -3,10 +3,10 @@ import SocialMediaLinks from './SocialMediaLinks';
 
 export const Footer = () => (
     <div style={{ display: 'grid', gridTemplateColumns: '50% 50%', padding: '5% 10% 0% 5%' }}>
-        <div style={{ textAlign: 'left', padding: '5%'}}>
-            <a href="https://emily-h-erskine.github.io/portfolio/" style={{fontSize: '10px', textDecoration: 'none'}}>Copyright © 2024 Emily-Huong Erskine</a>
+        <div style={{ textAlign: 'left', padding: '5%' }}>
+            <a href="https://emily-h-erskine.github.io/portfolio/" style={{ fontSize: '10px', textDecoration: 'none' }}>Copyright © 2024 Emily-Huong Erskine</a>
         </div>
-        <div style={{ textAlign: 'right', padding:'5% 0 0 0' }}>
+        <div style={{ textAlign: 'right', padding: '5% 0 0 0' }}>
             <SocialMediaLinks />
         </div>
   </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { Header } from './CommonComponents/Header/Header';
 import { Footer } from './CommonComponents/Footer/Footer';
 import './index.css';
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet"></link>
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <Header />
     <App />
     <Footer />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
