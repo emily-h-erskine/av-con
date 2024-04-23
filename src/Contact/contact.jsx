4d480d8a4a97a4f838afe8755ab46f8e580e5f30
Emily Erskine <s00209630@mail.itsligo.ie>
2024-04-23 22:33:54 +0100

    Still not working

--- src/Contact/contact.jsx
+++ src/Contact/contact.jsx
@@ -3,33 +3,22 @@ import ContactUsForm from "./contactForm";
 
 export default function Contact() {
   return (
-    <div style={{ position: 'relative', padding: '0% 2% 5% 2%' }}>
-      <div
-        style={{
-          width: '100%',
-          height: '50%',
-          left: 0,
-          top: 0,
-          position: 'absolute',
-          mixBlendMode: 'darken',
-          background:
-            'linear-gradient(180deg, #07101D 0%, rgba(7, 16, 29, 0) 100%)'
-        }}
-      />
+
+    <div style={{ padding: '0% 2% 5% 2%' }}>
+    <div>
       <img
         src="https://www.aviationpress.co.uk/wp-content/uploads/2022/04/PC-12-Banner-scaled.jpg"
         alt="header image"
         style={{
-          position: 'relative',
-          zIndex: '-1',
           width: '100%',
           mixBlendMode: 'luminosity'
         }}
       />
-      <div style={{padding:'5%'}}>
-        <h1 style={{ textAlign: 'center', color: 'white' }}>Contact Us</h1>
-        <ContactUsForm />
-      </div>
     </div>
+    <div style={{ padding: '2% 0%' }}>
+      <h1 style={{ textAlign: "center" }}>Contact Us</h1>
+      <ContactUsForm />
+    </div>
+  </div>
   );
 }
--- src/ExhibitorRegistration/exhibitor.jsx
+++ src/ExhibitorRegistration/exhibitor.jsx
@@ -4,33 +4,20 @@ import ExhibitorRegistrationForm from "./exhibitorRegistrationForm";
 export default function ExhibitorRegistration() {
   return (
 
-    <div style={{ position: 'relative', padding: '0% 2% 5% 2%' }}>
+
+    <div style={{ padding: '0% 2% 5% 2%' }}>
     <div>
-    <div
-      style={{
-        width: '100%',
-        height: '30%',
-        left: 0,
-        top: 0,
-        position: 'absolute',
-        mixBlendMode: 'darken',
-        background:
-          'linear-gradient(180deg, #07101D 0%, rgba(7, 16, 29, 0) 100%)'
-      }}
-    />
-    <img
-      src="https://www.aviationpress.co.uk/wp-content/uploads/2022/04/PC-12-Banner-scaled.jpg"
-      alt="header image"
-      style={{
-        position: 'relative',
-        zIndex: '-1',
-        width: '100%',
-        mixBlendMode: 'luminosity'
-      }}
-    />
+      <img
+        src="https://www.aviationpress.co.uk/wp-content/uploads/2022/04/PC-12-Banner-scaled.jpg"
+        alt="header image"
+        style={{
+          width: '100%',
+          mixBlendMode: 'luminosity'
+        }}
+      />
     </div>
-    <div style={{padding:'5%'}}>
-      <h1 style={{textAlign: "center"}}>Register to be an Exhibitor at AvCon</h1>
+    <div style={{ padding: '2% 0%' }}>
+      <h1 style={{ textAlign: "center" }}>Register to be an Exhibitor at AvCon</h1>
       <ExhibitorRegistrationForm />
     </div>
   </div>
