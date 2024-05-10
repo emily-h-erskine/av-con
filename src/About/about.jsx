1b4dbd2491daf3cdc0ed2a7a44f7b0263ebb1082
Emily Erskine <s00209630@mail.itsligo.ie>
2024-05-10 12:47:15 +0100

    More about fixes

--- src/About/about.jsx
+++ src/About/about.jsx
@@ -24,7 +24,23 @@ export default function About() {
   return (
     <main className="flex min-h-screen flex-col p-4 md:p-8 lg:p-12">
       <div className="event-container">
+
         <h1 className="event-title text-3xl md:text-4xl lg:text-5xl xl:text-6xl">About AvCon</h1>
+
+        <br/><br/>
+
+        <iframe
+            className="w-full md:w-3/4 lg:w-2/3 xl:w-1/2"
+            height="500"
+            width="100%"
+            src={AVConPromoVideo}
+            title="AvCon Future of Aviation"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+      />
+
+        <br/><br/>
         <div style={{ textAlign: "center", padding: "2%" }}>
         Welcome to AvCon - The Future of Aviation! Taking place on November 7th at The Irish Air Corps, Casement Aerodrome, Baldonnel, Co. Dublin, AvCon is thrilled to present its signature event dedicated to all things aviation.
         Designed to ignite the imaginations of students from primary to tertiary education levels, AvCon offers a dynamic platform to explore career opportunities and delve into the exciting world of flight. Whether you're a curious primary schooler, a discerning secondary school student, especially in Transition Year, or a tertiary education enthusiast, AvCon promises an immersive experience like no other.
@@ -37,18 +53,6 @@ export default function About() {
         <a href="/BookTickets"><u>Register through our registration and ticket page.</u> </a>
         <br/><br/>
         For all other queries please contact us hello@avcon.ie
-
-        <br/><br/>
-          <iframe
-            className="w-full md:w-3/4 lg:w-2/3 xl:w-1/2"
-            height="630"
-            width="100%"
-            src={AVConPromoVideo}
-            title="AvCon Future of Aviation"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          />
           {/* <AboutTimeline aboutAVCon={aboutAVCon} /> */}
         </div>
       </div>
