d432ffbab6afbcb6ee4b928649687d61c5a51675
Emily Erskine <s00209630@mail.itsligo.ie>
2024-06-22 18:07:50 +0100

    Changes to the about page

--- src/About/about.jsx
+++ src/About/about.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import AboutTimeline from './aboutTimeline.jsx';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import AVConPromoVideo from './The Future of aviation.mp4';
 
 export default function About() {
@@ -22,44 +24,45 @@ export default function About() {
 
   return (
     <main className="flex min-h-screen flex-col p-4 md:p-8 lg:p-12">
-      <div className="event-container text-center">
+        <div className="event-container text-center">
 
         <h1 className="event-title text-3xl md:text-4xl lg:text-5xl xl:text-6xl mb-4">About AvCon</h1>
 
-        <iframe
-            className="w-full md:w-3/4 lg:w-2/3 xl:w-1/2"
-            height="500"
-            width="100%"
-            src={AVConPromoVideo}
-            title="AvCon Future of Aviation"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-      />
-
-        <div className="text-lg md:text-xl lg:text-2xl xl:text-3xl mt-6 px-2 md:px-4 lg:px-6">
-          <p>
-            Welcome to AvCon - The Future of Aviation! Taking place on November 7th at The Irish Air Corps, Casement Aerodrome, Baldonnel, Co. Dublin, AvCon is thrilled to present its signature event dedicated to all things aviation.
-            Designed to ignite the imaginations of students from primary to tertiary education levels, AvCon offers a dynamic platform to explore career opportunities and delve into the exciting world of flight. Whether you're a curious primary schooler, a discerning secondary school student, especially in Transition Year, or a tertiary education enthusiast, AvCon promises an immersive experience like no other.
-          </p>
-          <p className="mt-4">
-            Dubbed as the Comic Con of Aviation, AvCon brings together aviation enthusiasts, industry experts, and educational institutions to showcase the cutting-edge advancements and endless possibilities within the aviation sector. Join us at AvCon and let your dreams take flight!
-          </p>
-          <p className="mt-4">
-            Please note: AvCon is only facilitating education group bookings at the moment. 
-          </p>
-          <p className="mt-4">
-            <a href="/BookTickets" className="text-blue-500 underline">Register through our registration and ticket page.</a>
-          </p>
-          <p className="mt-4">
-            For all other queries please contact us at <a href="mailto:hello@avcon.ie" className="text-blue-500 underline">hello@avcon.ie</a>
-          </p>
-        </div>
-        
         {/* Uncomment this when AboutTimeline is ready */}
         {/* <AboutTimeline aboutAVCon={aboutAVCon} /> */}
-        
-      </div>
+
+        </div>
+
+      <Box sx={{ padding: { xs: '0 2%', md: '0 5%' } }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={7}>
+                <iframe
+                  className="w-full md:w-3/4 lg:w-2/3 xl:w-1/2"
+                  height="500"
+                  width="100%"
+                  src={AVConPromoVideo}
+                  title="AvCon Future of Aviation"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                />
+              </Grid>
+              <Grid item xs={12} md={5}>
+                <div className="text-lg md:text-xl lg:text-2xl xl:text-3xl mt-6 px-2 md:px-4 lg:px-6">
+                    Welcome to AvCon - The Future of Aviation! Taking place on November 7th at The Irish Air Corps, Casement Aerodrome, Baldonnel, Co. Dublin, AvCon is thrilled to present its signature event dedicated to all things aviation.
+                    Designed to ignite the imaginations of students from primary to tertiary education levels, AvCon offers a dynamic platform to explore career opportunities and delve into the exciting world of flight. Whether you're a curious primary schooler, a discerning secondary school student, especially in Transition Year, or a tertiary education enthusiast, AvCon promises an immersive experience like no other.
+                    <br/><br/>
+                    Dubbed as the Comic Con of Aviation, AvCon brings together aviation enthusiasts, industry experts, and educational institutions to showcase the cutting-edge advancements and endless possibilities within the aviation sector. Join us at AvCon and let your dreams take flight!
+                    <br/><br/>
+                    Please note: AvCon is only facilitating education group bookings at the moment. 
+                    <br/><br/>
+                    <a href="/BookTickets">Register through our registration and ticket page.</a>
+                    <br/><br/>
+                    For all other queries please contact us at <a href="mailto:hello@avcon.ie">hello@avcon.ie</a>
+                </div>
+              </Grid>
+            </Grid>
+          </Box>
     </main>
   );
 }
