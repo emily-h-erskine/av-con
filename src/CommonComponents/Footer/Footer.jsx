cb8aa776f2888633e4772bee9cb4965c5f96c2a4
Emily Erskine <s00209630@mail.itsligo.ie>
2024-07-17 09:24:32 +0100

    Adding a footer banner to see how it looks

--- src/CommonComponents/Footer/Footer.jsx
+++ src/CommonComponents/Footer/Footer.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import SocialMediaLinks from './SocialMediaLinks';
+import FooterBanner from './footerBanner.jpeg';
 
 export default function Footer(){
 
     return (
 
         <div style={{ padding: '5% 0% 0% 0%'}}>
+            <div style={{ textAlign: 'center', padding: '5% 2%'}}>
+                <img
+                    src={FooterBanner}
+                    alt="Footer Banner Image"
+                    style={{ width: '100%', mixBlendMode: 'luminosity', display: 'block',}}
+                />
+            </div>
             <div style={{ textAlign: 'center', padding: '0% 5%'}}>
                 AvCon 2024 - Charting Aviation's Tomorrow. Set for November 7th, 2024, at Casement Aerodrome, Baldonnel, Co. Dublin, the home of the Irish Air Corps. Registration is currently underway for both <a href="./ExhibitorRegistration">Exhibitors </a> and <a href="./BookTickets">Schools</a>. Kindly be aware that registration for this event is designated for school and tertiary education group bookings exclusively; individual registrations are not applicable at this time. Any individual inquiries should be directed to hello@avcon.ie.
             </div>
