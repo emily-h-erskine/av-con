77bbdc884023a65a345c59dda05ebd41793ef866
LeighMcG83 <leighmcguinness@hotmail.com>
2024-09-06 21:43:48 +0100

    EDIT: Footer layout

--- src/CommonComponents/Footer/Footer.jsx
+++ src/CommonComponents/Footer/Footer.jsx
@@ -1,33 +1,59 @@
-import React from 'react';
-import SocialMediaLinks from './SocialMediaLinks';
-import FooterBanner from './footerBanner.jpeg';
+import React from "react";
+import SocialMediaLinks from "./SocialMediaLinks";
+import FooterBanner from "./footerBanner.jpeg";
+import "../../index.css";
 
-export default function Footer(){
-
+export default function Footer() {
     return (
-
-        <div style={{ padding: '5% 0% 0% 0%'}}>
-            <div style={{ textAlign: 'center', padding: '5% 2%'}}>
-                <img
-                    src={FooterBanner}
-                    alt="Footer Banner Image"
-                    style={{ width: '100%', mixBlendMode: 'luminosity', display: 'block',}}
-                />
+        <div>
+            <div className="flex-col-center mt-4" style={{ padding: "0 5%" }}>
+                <a
+                    href="./BookTickets"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <img
+                        src={FooterBanner}
+                        alt="AvCon 2024 URL, admission details and email address with a aearoplane spec diagram background"
+                        style={{
+                            width: "100%",
+                            mixBlendMode: "luminosity",
+                            display: "block",
+                        }}
+                    />
+                </a>
             </div>
-            <div style={{ textAlign: 'center', padding: '0% 5%'}}>
-                AvCon 2024 - Charting Aviation's Tomorrow. Set for November 7th, 2024, at Casement Aerodrome, Baldonnel, Co. Dublin, the home of the Irish Air Corps. Registration is currently underway for both <a href="./ExhibitorRegistration">Exhibitors </a> and <a href="./BookTickets">Schools</a>. Kindly be aware that registration for this event is designated for school and tertiary education group bookings exclusively; individual registrations are not applicable at this time. Any individual inquiries should be directed to hello@avcon.ie.
-            </div>
-            <div style={{ display: 'grid', gridTemplateColumns: '50% 50%', padding: '0% 10% 2% 5%'}}>
-                <div style={{ textAlign: 'left'}}>
-                  <a href="https://emily-h-erskine.github.io/portfolio/" style={{ fontSize: '10px', textDecoration: 'none' }}>Copyright © 2024 Emily-Huong Erskine</a>
+            <div
+                className="flex-col-center"
+                style={{
+                    padding: "0% 10% 2% 5%",
+                }}>
+                <p
+                    className="my-2 "
+                    style={{ textAlign: "center", padding: "0% 5%" }}>
+                    AvCon 2024 - Charting Aviation's Tomorrow. Set for November
+                    7th, 2024, at Casement Aerodrome, Baldonnel, Co. Dublin, the
+                    home of the Irish Air Corps. Registration is currently
+                    underway for both{" "}
+                    <a href="./ExhibitorRegistration">Exhibitors </a> and{" "}
+                    <a href="./BookTickets">Schools</a>. Kindly be aware that
+                    registration for this event is designated for school and
+                    tertiary education group bookings exclusively; individual
+                    registrations are not applicable at this time. Any
+                    individual inquiries should be directed to hello@avcon.ie.
+                </p>
+                <div className="flex-col-center">
+                    <SocialMediaLinks />
+                    <a
+                        href="https://emily-h-erskine.github.io/portfolio/"
+                        className="copyright"
+                        style={{
+                            fontSize: "14px",
+                            textDecoration: "none",
+                        }}>
+                        Copyright © 2024 Emily-Huong Erskine
+                    </a>
                 </div>
-                <div style={{ textAlign: 'right'}}>
-                     <SocialMediaLinks />
-                </div>
             </div>
-
         </div>
-    )
-
-
-};
+    );
+}
--- src/CommonComponents/Footer/SocialMediaLinks.jsx
+++ src/CommonComponents/Footer/SocialMediaLinks.jsx
@@ -4,28 +4,39 @@ import {
     faFacebook,
     faInstagram,
     faTiktok,
-    faLinkedinIn
-  } from "@fortawesome/free-brands-svg-icons";
+    faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function SocialMediaLinks() {
-  return (
-    <div class="social-container">
-      <a href="https://www.facebook.com/profile.php?id=61552633058659"
-        className="facebook social"  style={{padding: '2%'}}>
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
-      </a>
-      <a href="https://instagram.com/avcon_ie?igshid=eTNvcHN4MHdkZHJr"
-        className="instagram social"  style={{padding: '2%'}}>
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
-      </a>
-      <a href="https://www.tictok.com/"
-        className="tictok social"  style={{padding: '2%'}}>
-        <FontAwesomeIcon icon={faTiktok} size="2x" />
-      </a>
-      <a href="https://www.linkedin.com/in/tyhub?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="LinkedIn social"  style={{padding: '2%'}}>
-        <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
-      </a>
-      
-    </div>
-  );
+    return (
+        <div className="flex-container">
+            <div style={{ width: "100%" }} class="flex-container mb-2">
+                <a
+                    className="mx-2"
+                    href="https://www.facebook.com/profile.php?id=61552633058659"
+                    style={{ padding: "2%" }}>
+                    <FontAwesomeIcon icon={faFacebook} size="3x" />
+                </a>
+                <a
+                    className="mx-2"
+                    href="https://instagram.com/avcon_ie?igshid=eTNvcHN4MHdkZHJr"
+                    style={{ padding: "2%" }}>
+                    <FontAwesomeIcon icon={faInstagram} size="3x" />
+                </a>
+                <a
+                    className="mx-2"
+                    href="https://www.tictok.com/"
+                    // https://www.tiktok.com/@tyhub_ie/video/7396372068608101664
+                    style={{ padding: "2%" }}>
+                    <FontAwesomeIcon icon={faTiktok} size="3x" />
+                </a>
+                <a
+                    className="mx-2"
+                    href="https://www.linkedin.com/in/tyhub?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                    style={{ padding: "2%" }}>
+                    <FontAwesomeIcon icon={faLinkedinIn} size="3x" />
+                </a>
+            </div>
+        </div>
+    );
 }
