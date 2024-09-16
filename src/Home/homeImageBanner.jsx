a4eea68cad40cd823ac7c7f8f645ba7ebe19b0f3
Leigh McGuinness <leighmcguinness@hotmail.com>
2024-09-16 16:18:51 +0100

    EDIT: New mobile hero section

--- src/Home/homeImageBanner.jsx
+++ src/Home/homeImageBanner.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import HeroMobile from "./Hero-mobile.png";
 
 export default function HomeImageBanner() {
     return (
         <div className="mb-3" style={{ position: "relative" }}>
-            <img
-                src="https://www.aviationpress.co.uk/wp-content/uploads/2022/04/PC-12-Banner-scaled.jpg"
-                alt="Aeroplanes in a hanger"
-                style={{
-                    width: "100%",
-                    mixBlendMode: "luminosity",
-                    display: "block",
-                }}
-            />
+            <picture>
+                {/* This will display on screens 600px wide or smaller */}
+                <source media="(max-width: 768px)" srcSet={HeroMobile} />
+
+                {/* This will display on screens larger than 600px */}
+                <source
+                    media="(min-width: 769px)"
+                    srcSet="https://www.aviationpress.co.uk/wp-content/uploads/2022/04/PC-12-Banner-scaled.jpg"
+                />
+
+                {/* Fallback image */}
+                <img
+                    src="./Hero-mobile.png"
+                    alt="Aeroplanes in a hanger"
+                    style={{
+                        width: "100%",
+                        height: "100vh",
+                        mixBlendMode: "luminosity",
+                        display: "block",
+                    }}
+                />
+            </picture>
+
             <div
                 style={{
                     position: "absolute",
@@ -24,39 +39,34 @@ export default function HomeImageBanner() {
                         "linear-gradient(180deg, var(--background-dark) 0%, rgba(7, 16, 29, 0) 100%)",
                 }}
             />
-            <button
-                type="button"
-                name="Book Tickets Button"
-                style={{
-                    padding: "1.5% 3%",
-                    position: "absolute",
-                    right: "10%",
-                    top: "65%",
-                }}
-                className="btn-primary">
-                <a
-                    href="./BookTickets"
-                    className="uppercase"
-                    style={{
-                        textDecoration: "none",
-                        color: "inherit",
-                    }}>
-                    Get Tickets
-                </a>
-            </button>
 
             <div
+                className="mb-3 hero-content-position"
                 style={{
-                    position: "absolute",
-                    top: "20%",
-                    left: "10%",
-                    width: "80%",
-                    fontSize: "5vw",
-                    fontWeight: "800",
                     color: "white",
-                    textAlign: "center",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "end",
+                    marginBottom: "var(--margin-large)",
                 }}>
-                Welcome to the Future of Aviation!
+                <p className="title">AvCon 2024</p>
+                <p className="sub-title mb-3">
+                    Welcome to the Future of Aviation!
+                </p>
+                <button
+                    type="button"
+                    name="Book Tickets Button"
+                    className="btn-primary">
+                    <a
+                        href="./BookTickets"
+                        className="uppercase"
+                        style={{
+                            textDecoration: "none",
+                            color: "inherit",
+                        }}>
+                        Get Tickets
+                    </a>
+                </button>
             </div>
         </div>
     );
