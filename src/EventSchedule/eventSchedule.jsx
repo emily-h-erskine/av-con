92f628db96cce1b1192fb1998e3ceca00b4cb822
Emily Erskine <s00209630@mail.itsligo.ie>
2024-11-02 11:58:42 +0000

    Adding the Event Schedule

--- src/EventSchedule/eventSchedule.jsx
+++ src/EventSchedule/eventSchedule.jsx
@@ -1,33 +1,13 @@
 import React from "react";
-import Timeline from "./timeline.jsx";
-import EventAVCon from "./EventAVCon.jpeg";
-import EventAVConXtra from "./EventAVConXtra.jpeg";
+import AVConSchedule from "./AvConSpeakerSchedule.png";
 
 export default function EventSchedule() {
-    const eventsData = [
-        {
-            date: "2024",
-            event: "AVCon",
-            description: "Description for Event 1",
-            imageSrc: EventAVCon,
-            imageAlt: "AVCon Event",
-        },
-        {
-            date: "2024",
-            event: "AVCon Xtra",
-            description: "Description for Event 2",
-            imageSrc: EventAVConXtra,
-            imageAlt: "AVCon Xtra",
-        },
-    ];
-
     return (
         <main className="flex min-h-screen flex-col p-24">
             <div className="event-container">
                 <h3 style={{ textAlign: "center" }}>Event Schedule</h3>
-                <h6>To be announced soon... </h6>
-                <br />
-                <Timeline events={eventsData} />
+                <br/><br/>
+                <img src={AVConSchedule} width={'100%'}/>
             </div>
         </main>
     );
