a6538670246b6a4330fe00f449f7d6198d057c69
Emily Erskine <s00209630@mail.itsligo.ie>
2024-11-06 23:32:21 +0000

    Adding the live stream to the event schedule page

--- src/EventSchedule/eventSchedule.jsx
+++ src/EventSchedule/eventSchedule.jsx
@@ -3,11 +3,43 @@ import AVConSchedule from "./AvConSpeakerSchedule.png";
 
 export default function EventSchedule() {
     return (
-        <main className="flex min-h-screen flex-col p-24">
-            <div className="event-container">
-                <h3 style={{ textAlign: "center" }}>Event Schedule</h3>
-                <br/><br/>
-                <img src={AVConSchedule} width={'100%'}/>
+        <main>
+            <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
+                <h3 style={{ textAlign: "center", marginBottom: "2rem" }}>Event Schedule</h3>
+
+                <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+                    <img 
+                        src={AVConSchedule} 
+                        alt="AVCon Speaker Schedule" 
+                        style={{ width: "100%", height: "auto" }}
+                    />
+                </div>
+
+                <div style={{ textAlign: "center" }}>
+                    <h6 style={{ marginBottom: "1rem" }}>LIVE STREAM on TY HUB YOUTUBE</h6>
+                    <div 
+                        style={{
+                            position: "relative", 
+                            paddingBottom: "56.25%", 
+                            height: 0, 
+                            overflow: "hidden", 
+                            maxWidth: "100%", 
+                            backgroundColor: "#000",
+                            borderRadius: "8px",
+                            marginBottom: "3rem"
+                        }}
+                    >
+                        <iframe 
+                            src="https://www.youtube.com/embed/7_AYMtVLbhI?si=ulFkMd-tNUI6KD3z" 
+                            title="YouTube video player" 
+                            frameBorder="0" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                            referrerPolicy="strict-origin-when-cross-origin" 
+                            allowFullScreen
+                            style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
+                        />
+                    </div>
+                </div>
             </div>
         </main>
     );
